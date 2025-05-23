05581228fc8a1a7807c5e434232fcc3c5f826f37
kravchend <kravchend@gmail.com>
2025-05-23 18:25:00 +0200

    Внесены поправки в стили и игнорирован secret

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,18 +9,18 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+from secrets.secret import DJANGO_SECRET_KEY
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@vv@1ktc8cw&vo&6r6#7a@164w9=8)1fjccl=smx*^&ig6)^cj'
+# SECURITY WARNING: keep the secrets key used in production secrets!
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
