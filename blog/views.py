426204fde5eec6d6532871d24c192407de2ee811
kravchend <kravchend@gmail.com>
2025-05-28 22:01:18 +0200

    Окончательные изменения в файле views.py, поиск допускает наличие ошибок при вводе от пользователя

--- blog/views.py
+++ blog/views.py
@@ -110,6 +110,9 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + \
+                            SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
                 similarity=TrigramSimilarity('title', query),
             ).filter(similarity__gt=0.1).order_by('-similarity')
